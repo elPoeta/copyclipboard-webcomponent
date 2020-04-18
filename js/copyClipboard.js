a818576f76d853ef0819026e6922c4514370dab1
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-04-18 19:52:03 -0300

    create template element

--- js/copyClipboard.js
+++ js/copyClipboard.js
@@ -1,3 +1,10 @@
+const copyClipBoardTemplate = document.createElement('template');
+copyClipBoardTemplate.innerHTML = 
+  `<main>  
+   <slot name="my-text" id="type">Put your text here!</slot>
+   <button id="btn-copy">copy</button>
+  </main>`;
+  
 class CopyClipboard extends HTMLElement {
   constructor() {
     super();
